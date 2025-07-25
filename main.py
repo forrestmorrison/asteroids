7c4d97ec426b086f6d39e9b8c168c34492ed95f7
forrestmorrison <forrestmorrison@gmail.com>
2025-07-25 17:15:34 -0500

    Fixed player.py, added Player object to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 pygame.init()
@@ -12,17 +13,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0, 0, 0), None, 0)
+        screen.fill("black", None, 0)
+
+        player.draw(screen)
         
         pygame.display.flip()
 
         dt = game_clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
