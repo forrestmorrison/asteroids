5e3e35cba6a83cba458365045779b8e6013ae6f1
forrestmorrison <forrestmorrison@gmail.com>
2025-07-24 19:53:29 -0500

    created game_clock with pygame Clock object

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+game_clock = pygame.time.Clock()
+
+dt = 0
 
 def main():
     print("Starting Asteroids!")
@@ -13,8 +16,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0), None, 0)
+        
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
