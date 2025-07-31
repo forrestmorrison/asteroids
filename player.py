767e82be1a303a5d1ff5848a2ee2fc246c293f29
forrestmorrison <forrestmorrison@gmail.com>
2025-07-31 11:20:30 -0500

    added PLAYER_SHOOT_COOLDOWN to constants.py, added rate limiting functionality to player.py

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,7 +31,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position)
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity = forward * PLAYER_SHOOT_SPEED
+        shot.velocity = forward * PLAYER_SHOOT_SPEED    
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -43,5 +44,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if self.timer <= 0 and keys[pygame.K_SPACE]:
             self.shoot()
+            self.timer = PLAYER_SHOOT_COOLDOWN
+        if self.timer > 0:
+            self.timer -= dt
